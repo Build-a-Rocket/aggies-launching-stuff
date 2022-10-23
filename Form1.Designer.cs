4c3999f2919c589cc3b7c9a76d19506447d762bc
Dharmik-P45 <113470137+Dharmik-P45@users.noreply.github.com>
2022-10-23 15:49:06 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace ExampleGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.realTimeChart1 = new ExampleGUI.RealTimeChart();
+            this.realTimeChart2 = new ExampleGUI.RealTimeChart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.saveFileButton);
+            this.panel1.Controls.Add(this.loadFileButton);
+            this.panel1.Controls.Add(this.fileNameBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1600, 81);
+            this.panel1.TabIndex = 0;
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Location = new System.Drawing.Point(398, 19);
+            this.saveFileButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(150, 44);
+            this.saveFileButton.TabIndex = 2;
+            this.saveFileButton.Text = "Save File";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFile_Clicked);
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(236, 19);
+            this.loadFileButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.Size = new System.Drawing.Size(150, 44);
+            this.loadFileButton.TabIndex = 1;
+            this.loadFileButton.Text = "Load File";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.loadFile_Clicked);
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.Location = new System.Drawing.Point(24, 23);
+            this.fileNameBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.Size = new System.Drawing.Size(196, 31);
+            this.fileNameBox.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.AcceptsTab = true;
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputBox.Location = new System.Drawing.Point(800, 54);
+            this.outputBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(782, 673);
+            this.outputBox.TabIndex = 1;
+            // 
+            // inputBox
+            // 
+            this.inputBox.AcceptsReturn = true;
+            this.inputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputBox.Location = new System.Drawing.Point(6, 54);
+            this.inputBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(782, 673);
+            this.inputBox.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel1, 2);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.inputBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.outputBox, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 398);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1588, 380);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(800, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(782, 48);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(782, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.realTimeChart1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.realTimeChart2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 81);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1600, 784);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // realTimeChart1
+            // 
+            this.realTimeChart1.BackColor = System.Drawing.Color.Gainsboro;
+            this.realTimeChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.realTimeChart1.Location = new System.Drawing.Point(0, 0);
+            this.realTimeChart1.Margin = new System.Windows.Forms.Padding(0);
+            this.realTimeChart1.Name = "realTimeChart1";
+            this.realTimeChart1.Size = new System.Drawing.Size(800, 392);
+            this.realTimeChart1.TabIndex = 4;
+            this.realTimeChart1.Load += new System.EventHandler(this.realTimeChart1_Load);
+            // 
+            // realTimeChart2
+            // 
+            this.realTimeChart2.BackColor = System.Drawing.Color.Gainsboro;
+            this.realTimeChart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.realTimeChart2.Location = new System.Drawing.Point(800, 0);
+            this.realTimeChart2.Margin = new System.Windows.Forms.Padding(0);
+            this.realTimeChart2.Name = "realTimeChart2";
+            this.realTimeChart2.Size = new System.Drawing.Size(800, 392);
+            this.realTimeChart2.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox fileNameBox;
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private RealTimeChart realTimeChart1;
+        private RealTimeChart realTimeChart2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ExampleGUI
+{
+    public partial class Form1 : Form
+    {
+        Random r;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            realTimeChart1.setTitle("Random Data 1");
+
+            realTimeChart1.AddChart("Points", "Number", "Random", SeriesChartType.FastLine);
+
+            realTimeChart2.setTitle("Random Data 2");
+
+            realTimeChart2.AddChart("Points", "Number", "Random", SeriesChartType.FastLine);
+            r = new Random();
+        }
+
+        private void loadFile_Clicked(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(fileNameBox.Text);
+
+            foreach (string line in lines)
+            {
+                outputBox.AppendText(line + "\r\n");
+            }
+        }
+
+        private void saveFile_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileNameBox.Text, inputBox.Text);
+        }
+        string[] numbers = File.ReadAllLines("TextFile1.txt");//Turning the text file into a list of strings
+        int index = 0; // make variable index to use to access elements from the above list.
+        private void timer_Tick(object sender, EventArgs e)// With each tick of timer, one data point is added to the graph
+                                                           // , and index increments by one to go to the next value in the list.
+        {
+            realTimeChart1.AddPoint(0, double.Parse(numbers[index]), "Random");
+            realTimeChart2.AddPoint(0, double.Parse(numbers[index]), "Random");
+            index++;
+        }
+
+        private void realTimeChart1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
